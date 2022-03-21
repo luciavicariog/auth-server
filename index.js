3f8b766975737756baf1e54461540752c0cc81cf
luciavicariog <luciavicariog@gmail.com>
2022-03-21 10:30:40 +0100

    authApp server

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./db/config');
+require('dotenv').config();
+
+// console.log( process.env);
+
+//Crear el servidor/aplicación express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// Directorio público
+app.use( express.static('public'))
+
+//CORS
+app.use(cors());
+
+// Lectura y parseo del body
+app.use(express.json());
+
+//Rutas
+app.use( '/api/auth', require('./routes/auth'));
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
