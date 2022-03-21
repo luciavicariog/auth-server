00b8d591d346702f00d4cb18aa22038368c98d97
luciavicariog <luciavicariog@gmail.com>
2022-03-21 11:27:19 +0100

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./db/config');
+const path = require ('path');
 require('dotenv').config();
 
 // console.log( process.env);
@@ -24,6 +25,10 @@ app.use(express.json());
 //Rutas
 app.use( '/api/auth', require('./routes/auth'));
 
+//Manejar demás rutas
+app.get( '*' , ( req, res ) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html' ));
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
